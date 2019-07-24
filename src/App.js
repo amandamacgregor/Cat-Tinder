fc327c2f7b18984448578c256b04928e6656787c
amandamacgregor <amanda.macgregor13.com>
2019-07-24 19:08:57 +0000

    working halfway through day 1 of project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
+import Header from './Header'
+import Home from './pages/Home'
+import Cats from './pages/Cats'
+import NewCat from './pages/NewCat'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props){
+    super(props)
+      this.state = {
+       cats: [
+        {
+          id: 1,
+          name: 'Morris',
+          age: 2,
+          enjoys: "Long walks on the beach."
+        },
+        {
+          id: 2,
+          name: 'Paws',
+          age: 4,
+          enjoys: "Snuggling by the fire."
+        },
+        {
+          id: 3,
+          name: 'Mr. Meowsalot',
+          age: 12,
+          enjoys: "Being in charge."
+        }
+        ]
+      }
+  }
+  
+  render() {
+    return (
+      <div>
+        <Router>
+          <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <Link to="/" class="navbar-brand">Home</Link>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+              <div class="collapse navbar-collapse" id="navbarColor02">
+                <ul class="navbar-nav mr-auto">
+                  <li class="nav-item">
+                    <Link to="/cats" class="nav-link" >Cats</Link>
+                  </li>
+                  <li class="nav-item">
+                  <Link to="/newcats" class="nav-link" >New Cat</Link>
+                  </li>
+                </ul>
+              </div>
+          </nav>
+           <Header />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/cats" render={(props) => <Cats cats={this.state.cats}/> }/>
+            <Route exact path="/newcat" component={NewCat} />
+          </Switch>
+        </Router>
+        
+       
+        
+      </div>
+      )
+  }
 }
-
-export default App;
+        // <Route path="/" userLoggedIn={this.userLoggedIn} component={LogIn}/>
+        // <Route path="feed" relatives={this.state.relatives} userName={this.state.userName} component={Feed}/>
