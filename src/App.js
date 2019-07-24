7b58e4776464a84e78192e23d294e391e7df2bb3
amandamacgregor <amanda.macgregor13.com>
2019-07-24 23:48:00 +0000

    removed hamburger menu from nav bar

--- src/App.js
+++ src/App.js
@@ -44,10 +44,7 @@ handleNewCat = (e) => {
         <Router>
           <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
             <Link to="/" class="navbar-brand">Home</Link>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-            </button>
-              <div class="collapse navbar-collapse" id="navbarColor02">
+              <div id="navbarColor02">
                 <ul class="navbar-nav mr-auto">
                   <li class="nav-item">
                     <Link to="/cats" class="nav-link" >Cats</Link>
--- src/pages/Cats.js
+++ src/pages/Cats.js
@@ -11,25 +11,36 @@ import '../App.css';
 export default class Cats extends Component {
   render() {
     return (
-      <div>
-        <Row>
-          <Col xs={12}>
-            <ListGroup>
+      <div id="cats-display">
             {this.props.cats.map((cat, index) =>{
               return (
-                <ListGroup.Item key={index}>
-                  <h4>
-                    <span className='cat-name'>{cat.name}</span> - <small className='cat-age'>{cat.age} years old</small>
-                  </h4>
-                    <span className='cat-enjoys'>{cat.enjoys}</span>
-                  </ListGroup.Item>
+                  <div class="card text-white bg-info mb-3">
+                    <div class="card-header">Name: {cat.name}</div>
+                    <div class="card-body">
+                      <h4 class="card-title">Age: {cat.age}</h4>
+                      <p class="card-text">About: {cat.enjoys}</p>
+                    </div>
+                  </div>
                 )
               })}
-            </ListGroup>
-          </Col>
-        </Row>
-
       </div>
       )
   }
 }
+
+// <ListGroup.Item key={index}>
+//                   <h4>
+//                     <span className='cat-name'>{cat.name}</span> - <small className='cat-age'>{cat.age} years old</small>
+//                   </h4>
+//                     <span className='cat-enjoys'>{cat.enjoys}</span>
+//                   </ListGroup.Item>
+                  
+                  
+                  
+// <div class="card text-white bg-info mb-3" style="max-width: 20rem;">
+//   <div class="card-header">Header</div>
+//   <div class="card-body">
+//     <h4 class="card-title">Info card title</h4>
+//     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+//   </div>
+// </div>
